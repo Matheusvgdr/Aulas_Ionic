83eae1ed82288e88ceccca58254d90cf35cfa56a
Técnico Informática 2021.1 <51127512021.1@senacrjedu.local>
2022-12-16 11:52:11 -0300

    Continuação da toDo App

--- toDO/src/app/home/home.page.ts
+++ toDO/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ToastController } from '@ionic/angular';
+import { format } from 'path';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,13 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private alertaControle: AlertController, private toast: ToastController ) {}
 
-  constructor(private alertaControle: AlertController) {}
+  mensagens = [
+    {mensagem: 'Comprar pão'},
+    {mensagem: 'Tomar banho'},
+    {mensagem: 'Escovar os dentes'}
+  ]
 
   async adicionarTarefa(){
     const ALERTA = await this.alertaControle.create({
@@ -29,8 +35,8 @@ export class HomePage {
         },
         {
           text: 'Adicionar',
-          handler: () => {
-            console.log('adicionado');
+          handler: (form) => {
+            this.exibirValorInput(form.tarefa);
           }
         }
       ]
@@ -39,7 +45,62 @@ export class HomePage {
     ALERTA.present();
   }
 
+  //* ----------- FUNÇÔES ----------------
+  //pode usar o async na frente da função para fazer o alert dentro da função "async exibirValorInput()"
+  async exibirValorInput(tarefaNova: string){
+
+    if(tarefaNova.trim().length < 1){
+      //this.exibirMensagem();
+      const TOAST = await this.toast.create({
+        message: 'Você não digitou uma tarefa',
+        duration: 2000,
+        position: 'top',
+        color: 'dark'
+      });
+  
+      TOAST.present();
+
+    }else{
+      this.mensagens.push({mensagem: tarefaNova});
+    }
+  }
+
+  async apagar(){
+    const ALERTA = await this.alertaControle.create({
+      header: 'Deseja realmente excluir?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+            console.log('cancelou');
+          }
+        },
+        {
+          text: 'Sim',
+          handler: () => {
+            
+           // this.mensagens.splice(1);
+           // console.log(this.mensagens.indexOf({mensagem: "comprar"}));
+            this.mensagens.forEach( x => this.mensagens.splice(this.mensagens.indexOf(x)));
+          }
+        }
+      ]
+    });
+
+    ALERTA.present();
+  }
   ler(){
     
   }
+
+ /* async exibirMensagem() {
+    const ALERT = await this.alertaControle.create({
+      header: 'Alerta',
+      subHeader: 'Campo vazio!',
+      message: 'Você não digitou uma tarefa',
+      buttons: ['OK'],
+    });
+
+    await ALERT.present();
+  }*/
 }
